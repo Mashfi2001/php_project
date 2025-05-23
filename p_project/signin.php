ae29edaeacdb23dc70f7376ac27cdb9bc76daf2d
Mashfi <mashfimahin2003@gmail.com>
2025-05-23 21:04:44 +0600

    Update signin.php

--- p_project/signin.php
+++ p_project/signin.php
@@ -1,18 +1,17 @@
 <?php
-session_start();
-require_once"database.php";
+require_once"database.php"; //intialize database
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
+if (isset($_POST['username']) && isset($_POST['password'])) {  //checks password and username was given
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM user where username='$username'and password = '$password'";
+    $sql = "SELECT * FROM user where username='$username'and password = '$password'"; //sql code for retriveing data
     
-    $result = mysqli_query($conn,$sql);
+    $result = mysqli_query($conn,$sql); //runs sql quary
 
-    if (mysqli_num_rows($result) != 0 ) {
+    if (mysqli_num_rows($result) != 0 ) { //check if the retrived data is empty
         //echo"Let him enter";
-        header("Location: home.php");
+        header("Location: home.php"); // redirects to home page
         
     }
     else{
